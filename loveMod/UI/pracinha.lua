38dba31a7fb2b1220f7755129151fad161dae36f
mbanasik <adf.rg@o2.pl>
2021-01-11 18:03:15 +0100

    [FIX] rifleman to Pracinha upgrade rifleman upgraded to Pracinha will now correctly recieve Golden Age Combat Bonus

--- loveMod/UI/pracinha.lua
+++ loveMod/UI/pracinha.lua
@@ -5,25 +5,27 @@
 local unitPromotionPracinhaActiveID	= GameInfoTypes["PROMOTION_PRACINHA_ACTIVE"]
 local unitPromotionPracinhaID		= GameInfoTypes["PROMOTION_PRACINHA"]
 
+local unitTypePracinha = GameInfoTypes["UNIT_BRAZILIAN_PRACINHA"]
+
 function brazilian_Pracinha_PlayerDoTurn(playerID)
 	local player = Players[playerID]
 
 	if (not player:IsAlive()) then return end
 	if player:IsBarbarian() then return end
-	
+
 	for unit in player:Units() do
-		if (unit and (unit:IsHasPromotion(unitPromotionPracinhaID) or unit:IsHasPromotion(unitPromotionPracinhaActiveID))) then
+		if (unit and (unit:GetUnitType() == unitTypePracinha or unit:IsHasPromotion(unitPromotionPracinhaActiveID) or unit:IsHasPromotion(unitPromotionPracinhaID))) then
 			if (player:IsGoldenAge()) then
 				if (not unit:IsHasPromotion(unitPromotionPracinhaActiveID)) then
 					unit:SetHasPromotion(unitPromotionPracinhaActiveID, true)
 					unit:SetHasPromotion(unitPromotionPracinhaID, false)
 				end
 			else
-				if  (unit:IsHasPromotion(unitPromotionPracinhaActiveID)) then
+				if  (not unit:IsHasPromotion(unitPromotionPracinhaID)) then
 					unit:SetHasPromotion(unitPromotionPracinhaActiveID, false)
 					unit:SetHasPromotion(unitPromotionPracinhaID, true)
 				end
-			end	
+			end
 		end
 	end
 end
