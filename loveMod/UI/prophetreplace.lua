587251dfbde6b2957122514577dcf3924207f324
mbanasik <adf.rg@o2.pl>
2021-02-14 21:41:23 +0100

    [MOD] Austria Buff +25% influence from gold gifts to city stats +1 culture from Gardens, Operas, Zoos

--- loveMod/UI/prophetreplace.lua
+++ loveMod/UI/prophetreplace.lua
@@ -534,6 +534,22 @@ end)
 -------------------- DUMMY POLICIES ----------------------
 ----------------------------------------------------------
 
+-- Austria dummy policy
+function DummyPolicyAustria(player)
+    for playerID, player in pairs(Players) do
+        local player = Players[playerID];
+        if player:GetCivilizationType() == GameInfoTypes["CIVILIZATION_AUSTRIA"] then
+            if not player:HasPolicy(GameInfoTypes["POLICY_DUMMY_AUSTRIA"]) then
+
+                player:SetNumFreePolicies(1)
+                player:SetNumFreePolicies(0)
+                player:SetHasPolicy(GameInfoTypes["POLICY_DUMMY_AUSTRIA"], true)
+            end
+        end
+    end
+end
+Events.SequenceGameInitComplete.Add(DummyPolicyAustria)
+
 -- Mexico dummy policy
 
 print("dummy policy loaded - Mexico")
