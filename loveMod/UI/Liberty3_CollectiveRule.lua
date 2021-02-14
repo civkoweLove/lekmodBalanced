7b639397594748bd0bf45b32b0f027f759581e53
mbanasik <adf.rg@o2.pl>
2021-02-14 20:17:50 +0100

    [MOD]Collective rule 1 happiness for 3 new cities

--- loveMod/UI/Liberty3_CollectiveRule.lua
+++ loveMod/UI/Liberty3_CollectiveRule.lua
@@ -1,11 +1,13 @@
--- Add Governor's Mansion to newly founded cities
+-- Add Statue of freedom to every third newly founded city
 function Liberty_CollectiveRule_OnFoundCity(iPlayer, iCityX, iCityY)
 	local player = Players[iPlayer]
 	if (player:HasPolicy(GameInfo.Policies["POLICY_COLLECTIVE_RULE"].ID)) then
-		for loopCity in player:Cities() do
-			if (loopCity:GetX() == iCityX) then
-				if (loopCity:GetY() == iCityY) then
-					loopCity:SetNumRealBuilding(GameInfoTypes["BUILDING_STATUE_OF_FREEDOM"], 1)
+		if (player:GetNumCities() % 3 == 0) then
+			for loopCity in player:Cities() do
+				if (loopCity:GetX() == iCityX) then
+					if (loopCity:GetY() == iCityY) then
+						loopCity:SetNumRealBuilding(GameInfoTypes["BUILDING_STATUE_OF_FREEDOM"], 1)
+					end
 				end
 			end
 		end
