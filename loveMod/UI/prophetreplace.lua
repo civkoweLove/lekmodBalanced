226b6a7883e5071e5f9d4ccfa7994d854831f7d4
mbanasik <adf.rg@o2.pl>
2021-02-15 11:15:41 +0100

    [FIX] Iroques Buff +1 faith from Deer and Maize no longer causes crashes Longhouse and Mohawk warrior now have correct stats Austria Dummy Policy moved to new file

--- loveMod/UI/prophetreplace.lua
+++ loveMod/UI/prophetreplace.lua
@@ -534,21 +534,7 @@ end)
 -------------------- DUMMY POLICIES ----------------------
 ----------------------------------------------------------
 
--- Austria dummy policy
-function DummyPolicyAustria(player)
-    for playerID, player in pairs(Players) do
-        local player = Players[playerID];
-        if player:GetCivilizationType() == GameInfoTypes["CIVILIZATION_AUSTRIA"] then
-            if not player:HasPolicy(GameInfoTypes["POLICY_DUMMY_AUSTRIA"]) then
 
-                player:SetNumFreePolicies(1)
-                player:SetNumFreePolicies(0)
-                player:SetHasPolicy(GameInfoTypes["POLICY_DUMMY_AUSTRIA"], true)
-            end
-        end
-    end
-end
-Events.SequenceGameInitComplete.Add(DummyPolicyAustria)
 
 -- Mexico dummy policy
 
