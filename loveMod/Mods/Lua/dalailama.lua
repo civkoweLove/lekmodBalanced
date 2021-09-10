71946b19cef979e44f7328c1a85bc6929847ee5e
mbanasik <adf.rg@o2.pl>
2021-09-10 19:08:10 +0200

    [MOD] dalailama nerf now gives 4 turn golden age

--- loveMod/Mods/Lua/dalailama.lua
+++ loveMod/Mods/Lua/dalailama.lua
@@ -36,7 +36,7 @@ function dalailamaExpended(iPlayer, iUnit, eUnit, iPlotX, iPlotY, bDelay, eByPla
     -- triggers if Dalailama was killed by founding religion etc. not by combat
     if (eUnit == iDalailama) and bDelay then
         local pPlayer = Players[iPlayer];
-        pPlayer:ChangeGoldenAgeTurns(7);
+        pPlayer:ChangeGoldenAgeTurns(4);
     end
 end
 
