a60bd25dc92859a12e9fd869f5df60439362b618
mbanasik <adf.rg@o2.pl>
2021-12-21 12:43:21 +0100

    [FIX] small fixes

--- loveMod/Mods/Lua/Moors.lua
+++ loveMod/Mods/Lua/Moors.lua
@@ -1,7 +1,7 @@
 include( "Utility");
 
 local iCiv = GameInfoTypes["CIVILIZATION_MC_LITE_MOOR"]
-local iBuilding = GameInfoTypes["BUILDING_MOORS_TRAIT"]
+local iBuilding = GameInfoTypes["BUILDING_MOORS_TRAIT_MEDIEVAL"]
 local iEra = GameInfoTypes["ERA_MEDIEVAL"]
 local bIsActive = JFD_IsCivilisationActive(iCiv)
 
@@ -23,6 +23,47 @@ function Moors_CityCaptured(oldPlayerID, bCapital, iPlotX, iPlotY, newPlayerID)
     end
 end
 
+function Moors_TechEnter_Medival(iTeam, iTech, bAdopted)
+	for playerID, player in pairs(Players) do
+		local player = Players[playerID];
+		
+		if player:GetCivilizationType() == iCiv then
+			if player:GetCurrentEra() == iEra then
+				for pCity in player:Cities() do
+					pCity:SetNumRealBuilding(iBuilding, 1);
+				end
+				GameEvents.PlayerCityFounded.Add(Moors_CityFounded)
+				GameEvents.CityCaptureComplete.Add(Moors_CityCaptured)
+				GameEvents.TeamSetHasTech.Add(Moors_TechEnter_Renaissance)
+				
+				iEra = GameInfoTypes["ERA_RENAISSANCE"]
+				GameEvents.TeamSetHasTech.Remove(Moors_TechEnter_Medival)
+			end
+		end
+	end
+end
+
+function Moors_TechEnter_Renaissance(iTeam, iTech, bAdopted)
+	for playerID, player in pairs(Players) do
+		local player = Players[playerID];
+		
+		if player:GetCivilizationType() == iCiv then
+			if player:GetCurrentEra() == iEra then
+				for pCity in player:Cities() do
+					pCity:SetNumRealBuilding(iBuilding, 0);
+				end
+				iBuilding = GameInfoTypes["BUILDING_MOORS_TRAIT_RENAISSANCE"]
+				for pCity in player:Cities() do
+					pCity:SetNumRealBuilding(iBuilding, 1);
+				end
+				GameEvents.TeamSetHasTech.Add(Moors_TechLeave)
+				
+				GameEvents.TeamSetHasTech.Remove(Moors_TechEnter_Renaissance)
+			end
+		end
+	end
+end
+
 function Moors_TechLeave(iTeam, iTech, bAdopted)
 	for playerID, player in pairs(Players) do
 		local player = Players[playerID];
@@ -40,26 +81,6 @@ function Moors_TechLeave(iTeam, iTech, bAdopted)
 	end
 end
 
-function Moors_TechEnter(iTeam, iTech, bAdopted)
-	for playerID, player in pairs(Players) do
-		local player = Players[playerID];
-		
-		if player:GetCivilizationType() == iCiv then
-			if player:GetCurrentEra() == iEra then
-				for pCity in player:Cities() do
-					pCity:SetNumRealBuilding(iBuilding, 1);
-				end
-				GameEvents.PlayerCityFounded.Add(Moors_CityFounded)
-				GameEvents.CityCaptureComplete.Add(Moors_CityCaptured)
-				GameEvents.TeamSetHasTech.Add(Moors_TechLeave)
-				
-				GameEvents.TeamSetHasTech.Remove(Moors_TechEnter)
-			end
-		end
-	end
-end
-
-
 if bIsActive then
-	GameEvents.TeamSetHasTech.Add(Moors_TechEnter)
+	GameEvents.TeamSetHasTech.Add(Moors_TechEnter_Medival)
 end
